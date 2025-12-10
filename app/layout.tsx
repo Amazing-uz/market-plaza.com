6b690029342b5837df4ee00a6d701b83d5d6d862
abroranvarov01 <abrorxoja.anvarov88@gmail.com>
2025-12-10 16:54:06 +0500

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,19 +15,9 @@ export const metadata: any = {
 	icons: {
 		icon: [
 			{
-				url: '/icon-light-32x32.png',
-				media: '(prefers-color-scheme: light)',
-			},
-			{
-				url: '/icon-dark-32x32.png',
-				media: '(prefers-color-scheme: dark)',
-			},
-			{
-				url: '/icon.svg',
-				type: 'image/svg+xml',
-			},
+				url: '/plaza.png',
+			}
 		],
-		apple: '/apple-icon.png',
 	},
 }
 
